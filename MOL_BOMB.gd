35d1589afd332dbdce977e927b70802f18825835
Topguy100 <136177293+Topguy100@users.noreply.github.com>
2024-09-13 20:06:57 +0100

    Add files via upload

---
+++ MOL_BOMB.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("FLOOR"):
+		queue_free() # Replace with function body.
+		 # Replace with function body.
